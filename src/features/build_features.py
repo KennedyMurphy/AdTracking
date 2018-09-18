86fed9ebe902de2db0b0789d44a4695057b39a39
Kennedy Murphy <kennedy.murphy@fiddlehead.io>
2018-09-18 08:36:55 -0300

    Added convert_time function with tests.

---
+++ src/features/build_features.py
@@ -0,0 +1,20 @@
+import numpy as np
+from math import pi
+from datetime import datetime
+
+def convert_time(dt):
+    """ Converts the provided datetime to a tuple that represents
+        the coordinates on a clock.
+
+        :param dt:      Datetime object to be converted
+
+        :return:        tuple of clock coordinates
+    """
+
+    if not isinstance(dt, datetime):
+        raise ValueError("Expected datetime object. Gor class [{}]".format(
+            dt.__class__))
+    
+    rad = (360 + ((3 - dt.hour) / 12) * 360) * (pi / 180)
+
+    return (np.cos(rad), np.sin(rad))
